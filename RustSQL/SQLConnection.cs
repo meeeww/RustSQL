6c548c5f9916f62b9026319088c9f3347abbccd3
Zas <102923191+meeeww@users.noreply.github.com>
2024-01-05 23:47:16 +0100

    Version 1

--- RustSQL/SQLConnection.cs
+++ RustSQL/SQLConnection.cs
@@ -8,7 +8,7 @@ using Oxide.Core.Libraries.Covalence;
 
 namespace Oxide.Plugins
 {
-    [Info("SQLConnection", "Zas", "0.0.2")]
+    [Info("SQLConnection", "Zas", "1.0.0")]
     internal class SQLConnection : CovalencePlugin
     {
         private Core.MySql.Libraries.MySql mySql = new Core.MySql.Libraries.MySql();
@@ -27,6 +27,8 @@ namespace Oxide.Plugins
             config = Config.ReadObject<PluginConfig>();
         }
 
+        
+
         private void OnUserConnected(IPlayer player)
         {
             try
@@ -40,7 +42,6 @@ namespace Oxide.Plugins
                 {
                     if (results.Count == 0)
                     {
-                        Puts(results.Count.ToString());
                         var queryInsert = $"INSERT INTO PlayerData (userId, userName) VALUES (@0, @1)";
                         Sql insertCommand = Sql.Builder.Append(queryInsert, player.Id, player.Name);
 
@@ -62,6 +63,49 @@ namespace Oxide.Plugins
         }
 
         // PVPSTATS
+        private void OnPlayerDeath(BasePlayer victim, HitInfo info)
+        {
+            BasePlayer killer = info?.Initiator as BasePlayer;
+
+            if (killer == null) return;
+            if (victim.IsNpc || killer.IsNpc) return;
+
+            try
+            {
+                var connection = mySql.OpenDb(Config["Host"].ToString(), 3306, Config["DBName"].ToString(), Config["Username"].ToString(), Config["Password"].ToString(), this);
+
+                if(killer == victim)
+                {
+                    var queryKillerUpdate = $"UPDATE PlayerData SET suicides = suicides + 1 WHERE userId = @0";
+                    Sql updateKillerCommand = Sql.Builder.Append(queryKillerUpdate, killer.userID);
+
+                    mySql.Insert(updateKillerCommand, connection);
+
+                    mySql.CloseDb(connection);
+                }
+                else
+                {
+                    var queryKillerUpdate = $"UPDATE PlayerData SET kills = kills + 1 WHERE userId = @0";
+                    if (info.isHeadshot)
+                    {
+                        queryKillerUpdate = $"UPDATE PlayerData SET kills = kills + 1, headshots = headshots + 1 WHERE userId = @0";
+                    }
+                    var queryVictimUpdate = $"UPDATE PlayerData SET deaths = deaths + 1 WHERE userId = @0";
+                    Sql updateKillerCommand = Sql.Builder.Append(queryKillerUpdate, killer.userID);
+                    Sql updateVictimCommand = Sql.Builder.Append(queryVictimUpdate, victim.userID);
+
+                    mySql.Insert(updateKillerCommand, connection);
+                    mySql.Insert(updateVictimCommand, connection);
+
+                    mySql.CloseDb(connection);
+                }
+            }
+            catch (Exception ex)
+            {
+                Puts(ex.ToString());
+            }
+        }
+
         private void OnPlayerRevive(BasePlayer reviver, BasePlayer player)
         {
             try
@@ -69,7 +113,7 @@ namespace Oxide.Plugins
                 var connection = mySql.OpenDb(Config["Host"].ToString(), 3306, Config["DBName"].ToString(), Config["Username"].ToString(), Config["Password"].ToString(), this);
 
                 var queryReviverUpdate = $"UPDATE PlayerData SET revives = revives + 1 WHERE userId = @0";
-                var queryDownedUpdate = $"UPDATE PlayerData SET revives = revives + 1 WHERE userId = @0";
+                var queryDownedUpdate = $"UPDATE PlayerData SET revived = revived + 1 WHERE userId = @0";
                 Sql updateReviverCommand = Sql.Builder.Append(queryReviverUpdate, reviver.userID);
                 Sql updateDownedCommand = Sql.Builder.Append(queryDownedUpdate, player.userID);
 
@@ -84,11 +128,7 @@ namespace Oxide.Plugins
             }
         }
 
-        private void CanBeWounded(BasePlayer player, HitInfo info)
-        {
-            //Puts(info);
-        }
-
+        // OBJECTIVES STATS
         private void CanHackCrate(BasePlayer player)
         {
             try
@@ -108,10 +148,124 @@ namespace Oxide.Plugins
             }
         }
 
-        object OnPlayerAttack(BasePlayer attacker, HitInfo info)
+        private void OnEntityDeath(BaseCombatEntity target, HitInfo info)
         {
-            Puts("OnPlayerAttack works!");
-            return null;
+            if (info == null || target == null) return;
+            if (info.InitiatorPlayer == null || info.InitiatorPlayer == target) return;
+
+            if (target is ScientistNPC)
+            {
+                var player = info.InitiatorPlayer;
+                var ID = player.UserIDString;
+
+                var npcType = target.ShortPrefabName;
+
+                try
+                {
+                    var connection = mySql.OpenDb(Config["Host"].ToString(), 3306, Config["DBName"].ToString(), Config["Username"].ToString(), Config["Password"].ToString(), this);
+                    var selectQuery = "SELECT shortName FROM CientificosMatados WHERE shortName = @0 AND userId = @1";
+                    Sql selectCommand = Sql.Builder.Append(selectQuery, npcType, player.userID);
+
+                    mySql.Query(selectCommand, connection, results =>
+                    {
+                        if (results.Count == 0)
+                        {
+                            var queryInsert = $"INSERT INTO CientificosMatados (userId, shortName, quantity) VALUES (@0, @1, @2)";
+                            Sql insertCommand = Sql.Builder.Append(queryInsert, player.userID, npcType, 1);
+
+                            mySql.Insert(insertCommand, connection);
+
+                            mySql.CloseDb(connection);
+                        }
+                        else
+                        {
+                            var queryUpdate = $"UPDATE CientificosMatados SET quantity = quantity + 1 WHERE shortName = @0 AND userId = @1";
+                            Sql updateCommand = Sql.Builder.Append(queryUpdate, npcType, player.userID);
+
+                            mySql.Insert(updateCommand, connection);
+
+                            mySql.CloseDb(connection);
+                        }
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Puts(ex.ToString());
+                }
+            }
+        }
+
+        // RAID STATS
+        void OnExplosiveThrown(BasePlayer player, BaseEntity entity, ThrownWeapon item)
+        {
+            try
+            {
+                var connection = mySql.OpenDb(Config["Host"].ToString(), 3306, Config["DBName"].ToString(), Config["Username"].ToString(), Config["Password"].ToString(), this);
+                var selectQuery = "SELECT shortName FROM RaidData WHERE shortName = @0 AND userId = @1";
+                Sql selectCommand = Sql.Builder.Append(selectQuery, item.ShortPrefabName, player.userID);
+
+                mySql.Query(selectCommand, connection, results =>
+                {
+                    if (results.Count == 0)
+                    {
+                        var queryInsert = $"INSERT INTO RaidData (userId, shortName, quantity) VALUES (@0, @1, 1)";
+                        Sql insertCommand = Sql.Builder.Append(queryInsert, player.userID, item.ShortPrefabName);
+
+                        mySql.Insert(insertCommand, connection);
+
+                        mySql.CloseDb(connection);
+                    }
+                    else
+                    {
+                        var queryUpdate = $"UPDATE RaidData SET quantity = quantity + 1 WHERE shortName = @0 AND userId = @1";
+                        Sql updateCommand = Sql.Builder.Append(queryUpdate, item.ShortPrefabName, player.userID);
+
+                        mySql.Insert(updateCommand, connection);
+
+                        mySql.CloseDb(connection);
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                Puts(ex.ToString());
+            }
+        }
+
+        void OnRocketLaunched(BasePlayer player, BaseEntity entity)
+        {
+            try
+            {
+                var connection = mySql.OpenDb(Config["Host"].ToString(), 3306, Config["DBName"].ToString(), Config["Username"].ToString(), Config["Password"].ToString(), this);
+                var selectQuery = "SELECT shortName FROM RaidData WHERE shortName = @0 AND userId = @1";
+                Sql selectCommand = Sql.Builder.Append(selectQuery, entity.ShortPrefabName, player.userID);
+
+                mySql.Query(selectCommand, connection, results =>
+                {
+                    if (results.Count == 0)
+                    {
+                        var queryInsert = $"INSERT INTO RaidData (userId, shortName, quantity) VALUES (@0, @1, 1)";
+                        Sql insertCommand = Sql.Builder.Append(queryInsert, player.userID, entity.ShortPrefabName);
+
+                        mySql.Insert(insertCommand, connection);
+
+                        mySql.CloseDb(connection);
+                    }
+                    else
+                    {
+                        var queryUpdate = $"UPDATE RaidData SET quantity = quantity + 1 WHERE shortName = @0 AND userId = @1";
+                        Sql updateCommand = Sql.Builder.Append(queryUpdate, entity.ShortPrefabName, player.userID);
+
+                        mySql.Insert(updateCommand, connection);
+
+                        mySql.CloseDb(connection);
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                Puts(ex.ToString());
+            }
         }
 
         //FARM STATS
@@ -227,9 +381,6 @@ namespace Oxide.Plugins
                     }
                     else
                     {
-                        Puts(amount.ToString());
-                        Puts(item.info.itemid.ToString());
-                        Puts(player.userID.ToString());
                         var queryUpdate = $"UPDATE ItemsRecogidos SET quantity = quantity + @0 WHERE itemId = @1 AND userId = @2";
                         Sql updateCommand = Sql.Builder.Append(queryUpdate, amount, info.itemid, player.userID);
 
@@ -259,7 +410,6 @@ namespace Oxide.Plugins
             var amount = item.amount;
             var info = item.info;
 
-
             try
             {
                 var connection = mySql.OpenDb(Config["Host"].ToString(), 3306, Config["DBName"].ToString(), Config["Username"].ToString(), Config["Password"].ToString(), this);
@@ -279,9 +429,6 @@ namespace Oxide.Plugins
                     }
                     else
                     {
-                        Puts(amount.ToString());
-                        Puts(item.info.itemid.ToString());
-                        Puts(player.userID.ToString());
                         var queryUpdate = $"UPDATE ItemsRecogidos SET quantity = quantity + @0 WHERE itemId = @1 AND userId = @2";
                         Sql updateCommand = Sql.Builder.Append(queryUpdate, amount, info.itemid, player.userID);
 
@@ -297,5 +444,8 @@ namespace Oxide.Plugins
             }
             return null;
         }
+
+        // OTHER STATS
+
     }
 }
